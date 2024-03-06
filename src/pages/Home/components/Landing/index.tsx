8f73f27895732d037811cd0eb8a8a61c8d443301
Luciano Feder <lucianoo.feder@gmail.com>
2024-03-06 20:40:43 -0300

    fix: font size on landing screen for lower resolution

--- src/pages/Home/components/Landing/index.tsx
+++ src/pages/Home/components/Landing/index.tsx
@@ -19,20 +19,20 @@ export const Landing: React.FC = () => {
             id="home"
         >
             <Flex flexDirection="column" color="neutral.100">
-                <Text pl="5px" fontSize={["45px"]} fontWeight="700">Hi, I'm</Text>
+                <Text pl="5px" fontSize={{"2xl": "48px", base:"40px"}} fontWeight="700">Hi, I'm</Text>
                 <Heading
                     as="h1"
-                    fontSize={["120px"]}
+                    fontSize={{"2xl": "120px", base:"90px"}}
                     color="brand.500"
                     mb="-30px"
                     mt="50px"
                 >
                     LUCIANO FEDER
                 </Heading>
-                <Text pl="5px" fontSize={["40px"]}>Fullstack Developer</Text>
+                <Text pl="5px" fontSize={{"2xl": "40px", base:"32px"}}>Fullstack Developer</Text>
             </Flex>
             <Flex flexGrow={1} justifyContent="center">
-                <Box w="300px" h="300px" p="10px">
+                <Box w={{"2xl": "300px", base:"240px"}} h={{"2xl": "300px", base:"240px"}} p="10px">
                     <LogoIconSVG className="logo-icon"/>
                 </Box>
             </Flex>
